ec91f924d01566f3a8e4a907670fbebe13b8a078
Joshua <jkyejohnson@gmail.com>
2022-08-21 18:43:21 -0500

    created vowels class file

--- exercises/src/exercises/vowels.java
+++ exercises/src/exercises/vowels.java
@@ -4,10 +4,18 @@ public class vowels {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello please enter a sentence so I can count the vowels");
+		System.out.println("Hello please enter a sentence so I can count the vowels!");
 		Scanner scnr = new Scanner(System.in);
 		String sentence = scnr.nextLine();
-		
+		int count = 0;
+		for(int i = 0; i < sentence.length(); i++) {
+			sentence = sentence.toUpperCase();
+			if(sentence.charAt(i) == 'A' || sentence.charAt(i) == 'E' || sentence.charAt(i) == 'I' || sentence.charAt(i) == 'O' || sentence.charAt(i) == 'U') {
+				count++;
+			}
+		}
+		System.out.println("Your sentence has " + count + " vowels in it!");
 	}
 
 }
+ 
