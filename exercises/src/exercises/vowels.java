112cc8a3165d537086ac4b5d0b68674090a7294c
Joshua <jkyejohnson@gmail.com>
2022-08-21 17:03:28 -0500

    added a new java project

---
+++ exercises/src/exercises/vowels.java
@@ -0,0 +1,13 @@
+package exercises;
+import java.util.*;
+public class vowels {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello please enter a sentence so I can count the vowels");
+		Scanner scnr = new Scanner(System.in);
+		String sentence = scnr.nextLine();
+		
+	}
+
+}
