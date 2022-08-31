d0e52d6ace71ed161f56e3d673c168ad14ab4e66
JKJohnson0 <104327354+JKJohnson0@users.noreply.github.com>
2022-08-30 22:12:32 -0500

    added files

---
+++ registration/DatabaseConnection.java
@@ -0,0 +1,44 @@
+package registration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseConnection {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+	    final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	    final String DB_URL = "jdbc:mysql://awsmyql.cjcwogjvz1yx.us-west-1.rds.amazonaws.com:3306/Users";
+	    final String USER = "admin";
+	    final String PASS = "admin123";
+	    
+	    Connection conn = null;
+	    
+	    try {
+	    	Class.forName("com.mysql.jdbc.Driver");
+	    	System.out.println("Connecting to database...");
+	    	
+			conn = DriverManager.getConnection(DB_URL,USER,PASS);
+			
+		      System.out.println("Creating table in given database...");
+		      Statement stmt = conn.createStatement();
+		      //String sql = "INSERT INTO `testTable` (`AccountID`,`firstName`,`lastName`,`age`) VALUES ( 4,'Jay','C',21);";
+
+		      String sql = "CREATE TABLE TestTable " + 
+		                   "(SSN INTEGER not NULL, " +
+		                   " firstName VARCHAR(255), " + 
+		                   " lastName VARCHAR(255), " + 
+		                   " age INTEGER(30));"; 		     		      
+		      System.out.println(sql);
+		     // stmt.executeUpdate(sql);
+		      System.out.println("Created table in given database...");
+			
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
